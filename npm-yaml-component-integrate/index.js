07b0c0bec937f899156e9213c862e22b26c48c93
Manal Jaber-Shehayeb <manal.jabershehayeb@mirumagency.com>
2024-05-23 18:54:09 +0300

    :sparkles: Added npm library to integrate yaml
    made an npm library which takes a yaml file and make it json data

---
+++ npm-yaml-component-integrate/index.js
@@ -0,0 +1,14 @@
+
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yaml');
+
+const loadYaml = (filePath) => {
+  const fullPath = path.join(process.cwd(), filePath);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+  return YAML.parse(fileContents);
+};
+
+module.exports = {
+  loadYaml,
+};
